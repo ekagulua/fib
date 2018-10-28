4d4523f57df40d674fe92732ad95cdd63f1cae65
Ека <1211eka@gmail.com>
2018-10-28 01:58:53 +0000

    Created initial files

---
+++ index.js
@@ -0,0 +1,13 @@
+let n = prompt('n');
+function fibonacciCalc(n) {
+  var a = 1,
+    b = 1;
+  for (var i = 3; i <= n; i++) {
+    var c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+ alert(fibonacciCalc(n));
